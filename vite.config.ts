06d07fcc003e08f6c6bc0909dba8aa7ceb8fc07d
Bryan Levay <bryan@bryanlevay.com>
2024-11-13 23:01:14 -0800

    add volta pins, vite config for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => { 
+  const config: UserConfig = {
+    plugins: [react()],
+  }
+
+  if (mode === 'production') {
+    config.base = '/banner-3d/';
+  }
+
+  return config;
 })
